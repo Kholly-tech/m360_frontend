771a7ca1fed40489f78600df10eab7f469aedcdf
Kolawole Akintayo <kolawoleakintayok@gmail.com>
2025-10-09 10:50:30 +0100

    Clarity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import AppRoutes from "./routes/routes";
+import Clarity from "@microsoft/clarity";
+
 const App = () => {
+  const projectId = "tn02kkuk31";
+
+  Clarity.init(projectId);
   return (
     <div>
       <AppRoutes />
